4c1039fa26c5e594a26d74e144f262a2a14e14a2
RichardAPena <richardalexpena@hotmail.com>
2023-11-30 18:19:19 -0500

    version 2.3 release candidate

--- Mystic/Mods/Mystic/ScriptExtender/Lua/BootstrapServer.lua
+++ Mystic/Mods/Mystic/ScriptExtender/Lua/BootstrapServer.lua
@@ -1,12 +1,12 @@
 ---Make Phantom Knife penetrate 10 AC
----@param e EsvLuaBeforeDealDamageEvent
-local function PhantomKnifePenetrateAC(e)
-    if e.Hit.SpellId == "Target_Mystic_PhantomKnife" then
-        local roll = e.Hit.ConditionRolls[1]
-        -- roll.Difficulty = math.max(0, roll.Difficulty - 10)
-        roll.Difficulty = math.max(0, 10)
-        -- roll.Difficulty = 10
-    end
-end
+-- -@param e EsvLuaBeforeDealDamageEvent
+-- local function PhantomKnifePenetrateAC(e)
+--     if e.Hit.SpellId == "Target_Mystic_PhantomKnife" then
+--         local roll = e.Hit.ConditionRolls[1]
+--         -- roll.Difficulty = math.max(0, roll.Difficulty - 10)
+--         roll.Difficulty = math.max(0, 10)
+--         -- roll.Difficulty = 10
+--     end
+-- end
 
-Ext.Events.BeforeDealDamage:Subscribe(PhantomKnifePenetrateAC)
+-- Ext.Events.BeforeDealDamage:Subscribe(PhantomKnifePenetrateAC)
