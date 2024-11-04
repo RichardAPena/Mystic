af62eca91ddbde826bb1ff92e06e1d9aaf9ce599
RichardAPena <richardalexpena@hotmail.com>
2024-11-03 23:54:36 -0500

    wayyy too many changes

--- MysticExpansion/Mods/MysticExpansion/ScriptExtender/Lua/Server/PsychicDouble/PsychicDouble_OrderMove.lua
+++
@@ -1,7 +0,0 @@
-function PsychicDouble_UsingSpellAtPosition_Target_Mystic_PsychicDouble_OrderMove(caster, x, y, z, _, _, _, _)
-	local owner, clone = PsychicDouble_GetMysticAndActiveClone(caster)
-	if clone then
-		-- print("PsychicDouble: Requested clone teleport: " .. clone)
-        Osi.UseSpellAtPosition(clone, "Target_Mystic_PsychicDouble_Move_Clone", x, y, z)
-	end
-end
